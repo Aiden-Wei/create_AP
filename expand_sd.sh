f5f56a09cbf5c2e5333a29be8d1349a708c30e30
Aiden-Wei <45988164+Aiden-Wei@users.noreply.github.com>
2019-05-05 20:13:02 +0800

    Update expand_sd.sh

--- expand_sd.sh
+++ expand_sd.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-file="/boot/temporaryfiles.txt"
-if [ -f $file ]; then
+file_sd="/boot/temporaryfiles.txt"
+file_id="/etc/lobot/lobot_id"
+
+if [ -f $file_sd ]; then
   sudo rm $file
   sudo raspi-config nonint do_expand_rootfs
+  sudo chattr -i $file_id
+  sudo rm $file_id
   sleep 3
   sudo reboot
 fi
